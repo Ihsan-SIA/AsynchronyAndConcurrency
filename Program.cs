951cd549ccc4f64e001592fc98eeb5cf61b201cc
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-14 20:00:15 +0100

    Applied delay in the actual sense

--- Program.cs
+++ Program.cs
@@ -1,24 +1,32 @@
 ﻿var runAsync = new RunAsyncClass();
-runAsync.RunAsync();
+//await runAsync.RunAsync(1000, 2000);
+Task task1 = runAsync.RunAsync("Cook rice", 100000);
+Task task2 = runAsync.RunAsync("Warm eba", 10000);
+Task task3 = runAsync.RunAsync("Make tea", 1000);
+await Task.WhenAll(task1, task2, task3);
+
 public class DataFetcher
 {
-    public async Task<string> GetDataFromApiAsync()
+    public async Task<string> GetDataFromApiAsync(int delayMilliseconds)
     {
-        await Task.Delay(2000);
+        await Task.Delay(delayMilliseconds);
         return "Data from API";
     }
-    public async Task SaveDataToDbAsync(string data)
+    public async Task SaveDataToDbAsync(string data, int delayMilliseconds)
     {
-        await Task.Delay(1000);
+        await Task.Delay(delayMilliseconds);
         Console.WriteLine($"Saved: {data}");
     }
 }
 public class RunAsyncClass
 {
-    public async Task RunAsync()
+    public async Task RunAsync(string taskName, int delayTime)
     {
-        var fetcher = new DataFetcher();
-        string data = await fetcher.GetDataFromApiAsync();
-        await fetcher.SaveDataToDbAsync(data);
+        Console.WriteLine($"{taskName} started");
+        await Task.Delay(delayTime);
+        Console.WriteLine($"{taskName} completed after {delayTime/1000} seconds");
+        //var fetcher = new DataFetcher();
+        //string data = await fetcher.GetDataFromApiAsync(getTime);
+        //await fetcher.SaveDataToDbAsync(data, saveTime);
     }
 }
