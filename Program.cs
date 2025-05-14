17118b95751feaaa2ef6910f5c878e878a3ed4ca
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-14 18:55:16 +0100

    Simple example of async practice

--- Program.cs
+++ Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var runAsync = new RunAsyncClass();
+runAsync.RunAsync();
+public class DataFetcher
+{
+    public async Task<string> GetDataFromApiAsync()
+    {
+        await Task.Delay(2000);
+        return "Data from API";
+    }
+    public async Task SaveDataToDbAsync(string data)
+    {
+        await Task.Delay(1000);
+        Console.WriteLine($"Saved: {data}");
+    }
+}
+public class RunAsyncClass
+{
+    public async Task RunAsync()
+    {
+        var fetcher = new DataFetcher();
+        string data = await fetcher.GetDataFromApiAsync();
+        await fetcher.SaveDataToDbAsync(data);
+    }
+}
