5a4c46063abfb4bb6e728db80b42ffb893d547bb
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-15 00:15:12 +0100

    Trying to make a realtime stopwatch with my stopwatch app

--- Program.cs
+++ Program.cs
@@ -1,16 +1,50 @@
-﻿var runAsync = new RunAsyncClass();
-//await runAsync.RunAsync(1000, 2000);
-Task task1 = runAsync.RunAsync("Cook rice", 100000);
-Task task2 = runAsync.RunAsync("Warm eba", 10000);
-Task task3 = runAsync.RunAsync("Make tea", 1000);
-await Task.WhenAll(task1, task2, task3);
+﻿//var runAsync = new RunAsyncClass();
+////await runAsync.RunAsync(1000, 2000);
+//Task task1 = runAsync.RunAsync("Cook rice", 100000);
+//Task task2 = runAsync.RunAsync("Warm eba", 10000);
+//Task task3 = runAsync.RunAsync("Make tea", 1000);
+//await Task.WhenAll(task1, task2, task3);
 
-public class RunAsyncClass
+//public class RunAsyncClass
+//{
+//    public async Task RunAsync(string taskName, int delayTime)
+//    {
+//        Console.WriteLine($"{taskName} started");
+//        await Task.Delay(delayTime);
+//        Console.WriteLine($"{taskName} completed after {delayTime / 1000} seconds");
+//    }
+//}
+
+Task task1 = PrintTime();
+//Task task2 = ClearTime();
+await Task.WhenAll(task1);
+int num = 0;
+while (num < 11)
 {
-    public async Task RunAsync(string taskName, int delayTime)
-    {
-        Console.WriteLine($"{taskName} started");
-        await Task.Delay(delayTime);
-        Console.WriteLine($"{taskName} completed after {delayTime / 1000} seconds");
-    }
+    Console.Clear();
+    await Task.Delay(50);
+    num++;
 }
+
+async Task PrintTime()
+{
+    int num =0;
+    while (num<11)
+    {
+        Console.WriteLine(DateTime.Now);
+        await Task.Delay(1000);
+        num++;
+    }
+    
+}
+//async Task ClearTime()
+//{
+//    int num = 0;
+//    while (num < 11)
+//    {
+//        Console.Clear();
+//        await Task.Delay(50);
+//        num++;
+//    }
+    
+//}
