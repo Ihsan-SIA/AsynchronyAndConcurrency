4e90f2ec5557f50eb31f0f97124f4cf16277655c
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-14 20:22:38 +0100

    Back to basic async

--- Program.cs
+++ Program.cs
@@ -1,80 +1,16 @@
-﻿//var runAsync = new RunAsyncClass();
-////await runAsync.RunAsync(1000, 2000);
-//Task task1 = runAsync.RunAsync("Cook rice", 100000);
-//Task task2 = runAsync.RunAsync("Warm eba", 10000);
-//Task task3 = runAsync.RunAsync("Make tea", 1000);
-//await Task.WhenAll(task1, task2, task3);
+﻿var runAsync = new RunAsyncClass();
+//await runAsync.RunAsync(1000, 2000);
+Task task1 = runAsync.RunAsync("Cook rice", 100000);
+Task task2 = runAsync.RunAsync("Warm eba", 10000);
+Task task3 = runAsync.RunAsync("Make tea", 1000);
+await Task.WhenAll(task1, task2, task3);
 
-//public class RunAsyncClass
-//{
-//    public async Task RunAsync(string taskName, int delayTime)
-//    {
-//        Console.WriteLine($"{taskName} started");
-//        await Task.Delay(delayTime);
-//        Console.WriteLine($"{taskName} completed after {delayTime / 1000} seconds");
-//    }
-//}
-
-
-var stopwatch = new StopWatch();
-stopwatch.Menu();
-
-public class StopWatch
+public class RunAsyncClass
 {
-    public async Task PrintTime(string time, int delayTime)
+    public async Task RunAsync(string taskName, int delayTime)
     {
-        var watch = new Run();
-        time = watch.Start(DateTime.Now).ToString();
-        Console.WriteLine(time);
+        Console.WriteLine($"{taskName} started");
         await Task.Delay(delayTime);
-        Console.Clear();
-    }
-    public void Menu()
-    {
-        while (true)
-        {
-            var watch = new Run();
-            Console.WriteLine("Click enter key to start or any other key to end app");
-            var start = Console.ReadLine()!;
-            switch (start)
-            {
-                case "":
-                    Console.WriteLine("STOPWATCH IS COUNTING....");
-                    var startTime = watch.Start(DateTime.Now);
-                    Console.WriteLine("Press enter key to stop");
-                    while (true)
-                    {
-                        Task task1 = PrintTime(startTime.ToString(), 100);
-                    }
-                    var stop = Console.ReadKey()!;
-                    var endTime = char.IsWhiteSpace(stop.KeyChar) ? $"The duration time is: {watch.Stop(startTime)}s. Press any key to continue" : "\nInvalid! Click any key to start stopwatch again";
-                    Console.WriteLine(endTime);
-                    Console.ReadKey();
-                    Console.Clear();
-                    break;
-                default: return;
-            }
-
-        }
-    }
-    class Run
-    {
-        public DateTime StartTime { get; private set; }
-        public DateTime EndTime { get; set; }
-        public Run()
-        {
-            StartTime = DateTime.Now;
-        }
-        public DateTime Start(DateTime start)
-        {
-            StartTime = start;
-            return StartTime;
-        }
-        public TimeSpan Stop(DateTime start)
-        {
-            EndTime = start;
-            TimeSpan totalTime = DateTime.Now - EndTime;
-            return totalTime;
-        }
+        Console.WriteLine($"{taskName} completed after {delayTime / 1000} seconds");
     }
 }
