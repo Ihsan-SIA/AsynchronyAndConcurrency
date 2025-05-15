bc2a9f2f37b60fe5cfd9ae43b4a6b989a8989333
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-15 01:35:46 +0100

    Still trying to understand async

--- Program.cs
+++ Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿internal class Program
+{
+    private static async void Main(string[] args)
+    {
+        LongProcess();
+
+        ShortProcess();
+    }
+
+
+        static async void LongProcess()
+        {
+            Console.WriteLine("LongProcess Started");
+
+            await Task.Delay(4000); // hold execution for 4 seconds
+
+            Console.WriteLine("LongProcess Completed");
+
+        }
+
+        static void ShortProcess()
+        {
+            Console.WriteLine("ShortProcess Started");
+
+            //do something here
+
+            Console.WriteLine("ShortProcess Completed");
+        }
+    }
