bede1fc22edbcc290f416e81b7c30817df5a38f2
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-15 01:23:14 +0100

    Still trying to understand async

--- Comments.cs
+++ Comments.cs
@@ -19,9 +19,106 @@
         //    }
         //}
     }
+    public void TimeDelayAsync()
+    {
+        //Task task1 = PrintTime();
+        //Task task2 = ClearTime();
+        //await Task.WhenAll(task1, task2);
+
+
+        //async Task PrintTime()
+        //{
+        //    int num = 0;
+        //    while (num < 11)
+        //    {
+        //        await Task.Delay(1000);
+        //        Console.WriteLine(DateTime.Now);
+        //        num++;
+        //    }
+
+        //}
+        //async Task ClearTime()
+        //{
+        //    int num = 0;
+        //    while (num < 11)
+        //    {
+        //        await Task.Delay(1000);
+        //        Console.Clear();
+        //        num++;
+        //    }
+
+        //}
+        //}
+    }
     public void StopWatchAsync()
     {
+        //public class StopWatch
+        //{
+        //    public async Task ClearTime()
+        //    {
+        //        await Task.Delay(1000);
+        //        Console.Clear();
+        //    }
+        //    public async Task PrintTime()
+        //    {
+        //        Run run = new();
+        //        int rounds = 0;
+        //        while (rounds<20)
+        //        {
+        //            Console.WriteLine(run.Start(DateTime.Now));
+        //            Task task1 = run.Start(DateTime.Now);
+        //            //await Task.WhenAll(task1);
+        //            rounds++;
 
+        //        }
+        //    }
+        //    public void Menu()
+        //    {
+        //        while (true)
+        //        {
+        //            var watch = new Run();
+        //            Console.WriteLine("Click enter key to start or any other key to end app");
+        //            var start = Console.ReadLine()!;
+        //            switch (start)
+        //            {
+        //                case "":
+        //                    Console.WriteLine("STOPWATCH IS COUNTING....");
+        //                    var startTime = watch.Start(DateTime.Now);
+        //                    Console.WriteLine("Press enter key to stop");
+
+        //                    var stop = Console.ReadKey()!;
+        //                    var endTime = char.IsWhiteSpace(stop.KeyChar) ? $"The duration time is: {watch.Stop(startTime)}s. Press any key to continue" : "\nInvalid! Click any key to start stopwatch again";
+        //                    Console.WriteLine(endTime);
+        //                    Console.ReadKey();
+        //                    Console.Clear();
+        //                    break;
+        //                default: return;
+        //            }
+
+        //        }
+        //    }
+        //    class Run
+        //    {
+        //        public DateTime StartTime { get; private set; }
+        //        public DateTime EndTime { get; set; }
+        //        public Run()
+        //        {
+        //            StartTime = DateTime.Now;
+        //        }
+        //        public async Task<DateTime> Start(DateTime start)
+        //        {
+        //            StartTime = start;
+        //            await Task.Delay(1000);
+        //            return StartTime;
+        //        }
+        //        public TimeSpan Stop(DateTime start)
+        //        {
+        //            EndTime = start;
+        //            TimeSpan totalTime = DateTime.Now - EndTime;
+        //            return totalTime;
+        //        }
+        //    }
+        //}
     }
 }
 
@@ -30,102 +127,3 @@
 
 
 
-//Task task1 = PrintTime();
-//Task task2 = ClearTime();
-//await Task.WhenAll(task1, task2);
-
-
-//async Task PrintTime()
-//{
-//    int num = 0;
-//    while (num < 11)
-//    {
-//        await Task.Delay(1000);
-//        Console.WriteLine(DateTime.Now);
-//        num++;
-//    }
-
-//}
-//async Task ClearTime()
-//{
-//    int num = 0;
-//    while (num < 11)
-//    {
-//        await Task.Delay(1000);
-//        Console.Clear();
-//        num++;
-//    }
-
-//}
-
-
-
-
-
-//public class StopWatch
-//{
-//    public async Task ClearTime()
-//    {
-//        await Task.Delay(1000);
-//        Console.Clear();
-//    }
-//    public async Task PrintTime()
-//    {
-//        Run run = new();
-//        int rounds = 0;
-//        while (rounds<20)
-//        {
-//            Console.WriteLine(run.Start(DateTime.Now));
-//            Task task1 = run.Start(DateTime.Now);
-//            //await Task.WhenAll(task1);
-//            rounds++;
-
-//        }
-//    }
-//    public void Menu()
-//    {
-//        while (true)
-//        {
-//            var watch = new Run();
-//            Console.WriteLine("Click enter key to start or any other key to end app");
-//            var start = Console.ReadLine()!;
-//            switch (start)
-//            {
-//                case "":
-//                    Console.WriteLine("STOPWATCH IS COUNTING....");
-//                    var startTime = watch.Start(DateTime.Now);
-//                    Console.WriteLine("Press enter key to stop");
-
-//                    var stop = Console.ReadKey()!;
-//                    var endTime = char.IsWhiteSpace(stop.KeyChar) ? $"The duration time is: {watch.Stop(startTime)}s. Press any key to continue" : "\nInvalid! Click any key to start stopwatch again";
-//                    Console.WriteLine(endTime);
-//                    Console.ReadKey();
-//                    Console.Clear();
-//                    break;
-//                default: return;
-//            }
-
-//        }
-//    }
-//    class Run
-//    {
-//        public DateTime StartTime { get; private set; }
-//        public DateTime EndTime { get; set; }
-//        public Run()
-//        {
-//            StartTime = DateTime.Now;
-//        }
-//        public async Task<DateTime> Start(DateTime start)
-//        {
-//            StartTime = start;
-//            await Task.Delay(1000);
-//            return StartTime;
-//        }
-//        public TimeSpan Stop(DateTime start)
-//        {
-//            EndTime = start;
-//            TimeSpan totalTime = DateTime.Now - EndTime;
-//            return totalTime;
-//        }
-//    }
-//}
