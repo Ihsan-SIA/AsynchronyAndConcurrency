9e0671acb389574ac964d3d3b60ce022f5aeaad1
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-15 01:22:08 +0100

    Still trying to understand async

--- Comments.cs
+++ Comments.cs
@@ -1,46 +1,24 @@
-﻿
-//var runAsync = new RunAsyncClass();
-////await runAsync.RunAsync(1000, 2000);
-//Task task1 = runAsync.RunAsync("Cook rice", 100000);
-//Task task2 = runAsync.RunAsync("Warm eba", 10000);
-//Task task3 = runAsync.RunAsync("Make tea", 1000);
-//await Task.WhenAll(task1, task2, task3);
-
-//public class RunAsyncClass
-//{
-//    public async Task RunAsync(string taskName, int delayTime)
-//    {
-//        Console.WriteLine($"{taskName} started");
-//        await Task.Delay(delayTime);
-//        Console.WriteLine($"{taskName} completed after {delayTime / 1000} seconds");
-//    }
-//}
-
-
-
-
-LongProcess();
-
-ShortProcess();
-
-
-static async void LongProcess()
+﻿public class Comments
 {
-    Console.WriteLine("LongProcess Started");
+    public void NormalAsync()
+    {
+        //var runAsync = new RunAsyncClass();
+        ////await runAsync.RunAsync(1000, 2000);
+        //Task task1 = runAsync.RunAsync("Cook rice", 100000);
+        //Task task2 = runAsync.RunAsync("Warm eba", 10000);
+        //Task task3 = runAsync.RunAsync("Make tea", 1000);
+        //await Task.WhenAll(task1, task2, task3);
 
-    await Task.Delay(4000); // hold execution for 4 seconds
-
-    Console.WriteLine("LongProcess Completed");
-
-}
-
-static void ShortProcess()
-{
-    Console.WriteLine("ShortProcess Started");
-
-    //do something here
-
-    Console.WriteLine("ShortProcess Completed");
+        //public class RunAsyncClass
+        //{
+        //    public async Task RunAsync(string taskName, int delayTime)
+        //    {
+        //        Console.WriteLine($"{taskName} started");
+        //        await Task.Delay(delayTime);
+        //        Console.WriteLine($"{taskName} completed after {delayTime / 1000} seconds");
+        //    }
+        //}
+    }
 }
 
 
