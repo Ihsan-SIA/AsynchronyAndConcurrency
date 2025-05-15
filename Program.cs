8cf19879325bbf540b9020f4213a44d3d04630d7
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-15 01:49:42 +0100

    Still trying to understand async

--- Program.cs
+++ Program.cs
@@ -1,29 +1,28 @@
-﻿//LongProcess();
+﻿LongProcess();
 
-//ShortProcess();
+ShortProcess();
 
 
-//static async void LongProcess()
-//{
-//    Console.WriteLine("LongProcess Started");
-
-//    await Task.Delay(4000); // hold execution for 4 seconds 
-
-//    Console.WriteLine("LongProcess Completed");
-
-//}
-
-//static void ShortProcess()
-//{
-//    Console.WriteLine("ShortProcess Started");
-
-//    //do something here
-
-//    Console.WriteLine("ShortProcess Completed");
-//}
-AMethod();
-int AMethod()
+static async void LongProcess()
 {
-    Console.WriteLine("This is a method");
-    return 15;
+    async Task DoSomn()
+    {
+        Console.WriteLine("Do something");
+    }
+    Console.WriteLine("LongProcess Started");
+
+    await DoSomn();
+    //await Task.Delay(4000); // hold execution for 4 seconds 
+
+    Console.WriteLine("LongProcess Completed");
+
+}
+
+static void ShortProcess()
+{
+    Console.WriteLine("ShortProcess Started");
+
+    //do something here
+
+    Console.WriteLine("ShortProcess Completed");
 }
